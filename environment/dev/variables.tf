46753e982ac386f3c1b2d126ec807c9cfab75e6f
RakeshKSingh29 <singhrakesh2986@gmail.com>
2025-11-13 02:36:16 +0530

    db varibale removed

--- environment/dev/variables.tf
+++ environment/dev/variables.tf
@@ -261,25 +261,25 @@ variable "kubernetes_clusters" {
 
 #####################################
 
-variable "databases" {
-  type = map(object({
-    dbserver_name       = string
-    resource_group_name = string
-    location            = string
-    version             = string
-    tags                = optional(map(string))
-    sql_database_name   = string
-    key_vault_name      = string
-    username            = string
-    password            = string
-    collation           = optional(string)
-    license_type        = optional(string)
-    max_size_gb         = optional(string)
-    sku_name            = optional(string)
-    enclave_type        = optional(string)
+# variable "databases" {
+#   type = map(object({
+#     dbserver_name       = string
+#     resource_group_name = string
+#     location            = string
+#     version             = string
+#     tags                = optional(map(string))
+#     sql_database_name   = string
+#     key_vault_name      = string
+#     username            = string
+#     password            = string
+#     collation           = optional(string)
+#     license_type        = optional(string)
+#     max_size_gb         = optional(string)
+#     sku_name            = optional(string)
+#     enclave_type        = optional(string)
 
-  }))
-}
+#   }))
+# }
 ############################################
 
 # variable "bastion_hosts" {
